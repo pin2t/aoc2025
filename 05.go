f618c58d850e467e1da4d9e3cb126629411458cb
pin2t <pin2t@mail.ru>
2025-12-05 08:53:35 +0300

    day 5 better names

--- 05.go
+++ 05.go
@@ -6,41 +6,45 @@ import "strconv"
 import "fmt"
 import "sort"
 
+type rng struct { first, last int64 }
+
 func main() {
 	var scanner = bufio.NewScanner(os.Stdin)
-	var ranges = make([][]int64, 0)
+	var ranges = make([]rng, 0)
 	for scanner.Scan() {
 		var line = scanner.Text()
 		if line == "" { break }
-		var parts = strings.Split(line, "-")
-		var start, _ = strconv.ParseInt(parts[0], 10, 64)
-		var end, _ = strconv.ParseInt(parts[1], 10, 64)
-		ranges = append(ranges, []int64{ start, end })
+		var pair = strings.Split(line, "-")
+		var r rng
+		r.first, _ = strconv.ParseInt(pair[0], 10, 64)
+		r.last, _ = strconv.ParseInt(pair[1], 10, 64)
+		ranges = append(ranges, r)
 	}
 	var fresh, total = 0, int64(0)
 	for scanner.Scan() {
 		var id, _ = strconv.ParseInt(scanner.Text(), 10, 64)
 		for _, r := range ranges {
-			if r[0] <= id && id <= r[1] {
+			if r.first <= id && id <= r.last {
 				fresh++
 				break
 			}
 		}
 	}
 	sort.Slice(ranges, func(i, j int) bool {
-		return ranges[i][0] < ranges[j][0]
+		return ranges[i].first < ranges[j].first
 	})
-	var merged = make([][]int64, 1)
+	var merged = make([]rng, 1)
 	merged[0] = ranges[0]
 	for i := 1; i < len(ranges); i++ {
-		if ranges[i][0] <= merged[len(merged) - 1][1] {
-			merged[len(merged) - 1][1] = max(merged[len(merged) - 1][1], ranges[i][1])
+		var l = &merged[len(merged) - 1]
+		if ranges[i].first <= l.last {
+			l.last = max(l.last, ranges[i].last)
 		} else {
 			merged = append(merged, ranges[i])
 		}
 	}
 	for _, r := range merged {
-		total += r[1] - r[0] + 1
+		total += r.last - r.first + 1
 	}
 	fmt.Println(fresh, total)
 }
