1a71a84a3879fd040a4cfac0b3d42d08c45afbef
pin2t <pin2t@mail.ru>
2025-12-12 08:36:42 +0300

    day 10

--- 10.go
+++ 10.go
@@ -1,11 +1,11 @@
 package main
 import "fmt"
 import "regexp"
-import "strings"
 import "strconv"
 import "bufio"
 import "os"
 import "slices"
+import z3 "github.com/mitchellh/go-z3"
 
 func main() {
 	type machine struct {
@@ -15,25 +15,30 @@ func main() {
 	}
 	var machines = make([]machine, 0)
 	var reWire = regexp.MustCompile("\\(.*?\\)")
-	var reJolts = regexp.MustCompile("{.*?}")
+	var reParts = regexp.MustCompile("^\\[([.#]+)] ([()\\d, ]+) \\{([\\d,]+)}$")
 	var reNum = regexp.MustCompile("\\d+")
 	var scanner = bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		var line = scanner.Text()
-		var l = line[strings.Index(line, "[") + 1:strings.LastIndexByte(line, ']')];
-		var lights = make([]bool, len(l))
-		for i, c := range l { lights[i] = c == '#' }
-		var bs = reWire.FindAllString(line, -1)
+		var parts = reParts.FindStringSubmatch(line)
+		var lights = make([]bool, len(parts[1]))
+		for i, c := range parts[1] {
+			lights[i] = c == '#'
+		}
+		var bs = reWire.FindAllString(parts[2], -1)
 		var buttons = make([][]int, len(bs))
 		for i, b := range bs {
 			var ns = reNum.FindAllString(b, -1)
 			buttons[i] = make([]int, len(ns))
-			for j, n := range ns { buttons[i][j] = toint(n) }
+			for j, n := range ns {
+				buttons[i][j] = toint(n)
+			}
 		}
-		var js = reJolts.FindString(line)
-		var ns = reNum.FindAllString(js, -1)
+		var ns = reNum.FindAllString(parts[len(parts) - 1], -1)
 		var joltage = make([]int, len(ns))
-		for i, n := range ns { joltage[i] = toint(n) }
+		for i, n := range ns {
+			joltage[i] = toint(n)
+		}
 		machines = append(machines, machine { lights, buttons, joltage })
 	}
 	var presses = []int{ 0, 0 }
@@ -65,43 +70,49 @@ func main() {
 			}
 		}
 	}
-	//for _, m := range machines {
-	//	type state struct {
-	//		jlts []int
-	//		presses int
-	//	}
-	//	var init = make([]int, len(m.joltage))
-	//	for i, _ := range init {
-	//		init[i] = 0
-	//	}
-	//	var queue = make([]state, 0)
-	//	queue = append(queue, state{ init, 0 })
-	//	for len(queue) > 0 {
-	//		var st = queue[0]
-	//		queue = queue[1:]
-	//		fmt.Println(st.jlts, m.joltage, st.presses)
-	//		if slices.Equal(m.joltage, st.jlts) {
-	//			presses[1] += st.presses
-	//			break
-	//		}
-	//		for _, b := range m.buttons {
-	//			var stt = st
-	//			var exceed = false
-	//			for !exceed {
-	//				var jlts = make([]int, len(stt.jlts))
-	//				copy(jlts, stt.jlts)
-	//				for _, ji := range b {
-	//					jlts[ji]++
-	//					exceed = jlts[ji] > m.joltage[ji]
-	//				}
-	//				if !exceed {
-	//					queue = append(queue, state{ jlts, stt.presses + 1 })
-	//					stt = state{ jlts, stt.presses + 1 }
-	//				}
-	//			}
-	//		}
-	//	}
-	//}
+	var config = z3.NewConfig()
+	var ctx = z3.NewContext(config)
+	config.Close()
+	defer ctx.Close()
+	for _, m := range machines {
+		var s = ctx.NewSolver()
+		defer s.Close()
+		var zero = ctx.Int(0, ctx.IntSort())
+		var vars = make([]*z3.AST, len(m.buttons))
+		for i, _ := range m.buttons {
+			vars[i] = ctx.Const(ctx.Symbol(fmt.Sprintf("n%d", i)), ctx.IntSort())
+			s.Assert(vars[i].Gt(zero))
+		}
+		for i, j := range m.joltage {
+			var equation = ctx.Int(0, ctx.IntSort())
+			for k, b := range m.buttons {
+				var found = false
+				for _, out := range b {
+					if out == i {
+						found = true
+						break
+					}
+				}
+				if found {
+					equation.Add(vars[j])
+				}
+			}
+			equation.Eq(ctx.Int(j, ctx.IntSort()))
+			s.Assert(equation)
+		}
+		if v := s.Check(); v != z3.True {
+			fmt.Println("Unsolvable", m)
+		} else {
+			var model = s.Model()
+			var assignments = model.Assignments()
+			model.Close()
+			var pr = 0
+			for _, a := range assignments {
+				pr += a.Int()
+			}
+			presses[1] += pr
+		}
+	}
 	fmt.Println(presses)
 }
 
---
+++ go.mod
@@ -0,0 +1,5 @@
+module aoc2025
+
+go 1.25.4
+
+require github.com/mitchellh/go-z3 v0.0.0-20191228203228-4cbedeba863f // indirect
